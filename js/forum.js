5ae638d2adbb8b12b2620039d3adb63b35550c88
Gary O'Donoghue <Gary@Garys-MacBook-Air.local>
2015-08-18 22:06:49 +0100

    fixup comment/respond

--- js/forum.js
+++ js/forum.js
@@ -16,17 +16,13 @@ function displayPosts(){
           return el;
         })
 
-        var html = '</br></br></br>';
+        var html = '</br></br></br><div class = "container"><div class="row main-row">';
 
           for(var i=commentsArr.length-1; i>=0;i--){
-            html += '<br><div id="'+commentIndexMappings[i]+'" class="container"><b>';
+            html += '<div id="'+commentIndexMappings[i]+'" class="detailBox"><div class="titleBox"><label>';
             html += commentsArr[i].text;
-            html += '</b></br></br>';
-
-            html += '<textarea placeholder="respond to comment..." class="form-group" id="' + commentIndexMappings[i] + 'RespTxt" "rows="1" cols="100"></textarea></br>';
-            html += '<input class="form-group" id="'+ commentIndexMappings[i] +'" type="submit" value="Respond" onClick="submitResponse(event)">';
-
-            html += '</br></br>';
+            html += '</label></div><div class="commentBox"><p class="taskDescription">';
+            html += '</br>';
 
             if(commentsArr[i].responses != null){
 
@@ -40,14 +36,19 @@ function displayPosts(){
 
 
               for(var j = 0; j<responsesArr.length;j++){
-                html += '<li>' + responsesArr[j].text;
-                html += '</br>';
+                html += '<div class="actionBox"><ul class="commentList"><li><div class="commenterImage"><img src="http://lorempixel.com/50/50/people/6"></div><div class="commentText"><p class="">';
+                html += responsesArr[j].text;
+                html += '</p> <span class="date sub-text">on March 5th, 2014</span></div></li>';
               }
             }
 
-            html += '</div></form></br><hr>';
+            html += '</div>';
+            html += '<form class="form-inline" role="form">';
+            html += '<div class="form-group"><input id="' + commentIndexMappings[i] + 'RespTxt" class="form-control" type="text" placeholder="say something..."></div><div class="form-group" style="margin:10px"><input class="btn btn-danger" id="'+ commentIndexMappings[i] +'" type="submit" value="Respond" onClick="submitResponse(event)"></div></form>';
+            html += '</div></div></div></br><hr>';
           }
 
+        html+='</div></div>';
         $("#main").html(html);
       },
       error: function (msg, url, line) {
@@ -57,11 +58,12 @@ function displayPosts(){
 }
 
 function submitResponse(event){
+  debugger;
     var commentDiv = "#"+event.target.id;
     var respTxtDiv = "#"+event.target.id+"RespTxt";
     var responseText = $(respTxtDiv).val();
 
-      var url = 'https://glaring-torch-16.firebaseio.com/comments/'+event.target.id+'/responses.json';
+    var url = 'https://glaring-torch-16.firebaseio.com/comments/'+event.target.id+'/responses.json';
 
     $.ajax(
     {
@@ -71,10 +73,8 @@ function submitResponse(event){
       contentType: "application/json; charset=utf-8",
       dataType: "json",
       success: function (data) {
-        $(commentDiv).append("<div>"+responseText+"</div>");
-        console.log('commentId='+event.target.id);
-
         $(respTxtDiv).val('');
+        displayPosts();
       },
       error: function (msg, url, line) {
         alert('error');
@@ -97,8 +97,8 @@ $( "#postBtn" ).bind( "click", function(event, ui) {
       contentType: "application/json; charset=utf-8",
       dataType: "json",
       success: function (data) {
-        $("#main").prepend("<div><b>"+commentText+"</b></div></br><hr>")
         $("#commentTxtBox").val('');
+        displayPosts();
       },
       error: function (msg, url, line) {
         alert('error');
