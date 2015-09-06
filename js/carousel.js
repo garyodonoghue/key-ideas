380a90d5902831e247965aa189b444cd0dea78a8
Gary O'Donoghue <Gary@Garys-MacBook-Air.local>
2015-09-06 15:09:44 +0100

    changed logo in nav bar. change tab text colour to white

--- js/carousel.js
+++ js/carousel.js
@@ -1,7 +1,16 @@
-$('#myCarousel').on('slide.bs.carousel',function(){
-    $('#myCarousel .active .carousel-caption').fadeOut();
-});
+var carouselContainer = $('.carousel');
+var slideInterval = 3000;
 
-$('#myCarousel').on('slid.bs.carousel',function(){
-    $('#myCarousel .active .carousel-caption').fadeIn();
+function toggleCaption() {
+    $('.carousel-caption').hide();
+    var caption = carouselContainer.find('.active').find('.carousel-caption');
+    caption.delay(500).toggle("slide", {direction:'right'});
+}
+
+carouselContainer.carousel({
+    interval: slideInterval,
+    cycle: true,
+    pause: "hover"
+}).on('slid.bs.carousel', function() {
+    toggleCaption();
 });
