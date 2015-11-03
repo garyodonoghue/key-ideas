970dda3313403971882e1ab6f512b06f2bbc5926
Gary O'Donoghue <Gary@Garys-MacBook-Air.local>
2015-11-03 23:13:47 +0000

    extract nav bar out into common header. change about and contact pages to redirects, not staying on same page

--- js/index.js
+++ js/index.js
@@ -1,27 +1,3 @@
-function loadAbout(){
-    $("#body" ).empty();
-    $("#forumInfo").empty();
-    $("#forumComments" ).empty();
-    
-    $.get('/about.html', function(data) {
-      $("#body").html(data);
-    });
-
-    return false;
-}
-
-function loadContact(){
-  $("#body" ).empty();
-  $("#forumComments" ).empty();
-  $("#forumInfo").empty();
-
-  $.get('/contact.html', function(data) {
-    $("#body").html(data);
-  });
-
-  return false;
-}
-
 //set nav element as selected
 $(".nav a").on("click", function(){
    $(".nav").find(".active").removeClass("active");
