efaca9ffb2bb55528d2bdf37087debb84a5cc843
Gary O'Donoghue <Gary@unknown.home>
2016-01-16 18:26:29 +0000

    show user's name when logged in

--- js/login.js
+++ js/login.js
@@ -9,6 +9,7 @@ function loginFacebook(){
         console.log("Login Failed!", error);
       } else {
         console.log("Authenticated successfully with payload:", authData);
+        displayProfilePic();
       }
     }, {remember: "sessionOnly"});
   }
@@ -23,6 +24,7 @@ function loginTwitter(){
     ref.authWithOAuthPopup("twitter", function(error, authData) {
       if (error) {
         console.log("Login Failed!", error);
+        displayProfilePic();
       } else {
         console.log("Authenticated successfully with payload:", authData);
       }
@@ -40,9 +42,36 @@ function loginGoogle(){
     ref.authWithOAuthPopup("google", function(error, authData) {
       if (error) {
         console.log("Login Failed!", error);
+        displayProfilePic();
       } else {
         console.log("Authenticated successfully with payload:", authData);
       }
     }, {remember: "sessionOnly"});
   }
+}
+
+function displayProfilePic(){
+  //get the user profile pic from the auth token
+  var ref = new Firebase("https://glaring-torch-16.firebaseio.com");
+  var authToken = ref.getAuth();
+  var authProvider = authToken.provider;
+
+  var profileImageURL;
+  var fullName;
+
+  if(authProvider == "google"){
+      fullName = authToken.google.displayName;
+      profileImageURL = authToken.google.profileImageURL
+  }
+  else if(authProvider == "facebook"){
+    fullName = authToken.facebook.displayName;
+    profileImageURL = authToken.facebook.profileImageURL
+  }
+  else if(authProvider == "twitter"){
+    fullName = authToken.twitter.displayName;
+    profileImageURL = authToken.twitter.profileImageURL
+  }
+
+  var firstName = fullName.split(" ")[0];
+  document.getElementById("signInDropDown").innerHTML = '<p style="color: #fff;padding-top: 4px; font-size:18px; font-family:font-family: "Helvetica Neue"; display:block">Hello, '+firstName+'</p>';
 }
